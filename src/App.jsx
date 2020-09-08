7fa68a63b70e1d0ba527846ec872f32b5f82662b
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-09-08 20:09:20 +0600

    change heading

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 import Sdata from './Sdata'
 
 const App=()=>(<React.Fragment>
-      <h1 className="heading_style">Top Netflix Series</h1>
+      <h1 className="heading_style">Top Netflix Series of 2020</h1>
       {Sdata.map((val)=>{
       return(<Card
       key={val.id}
